b36ba8091a050560df51a1572e0dda3a3e73dacb
Natalia Hetmańska <hetmanska.natalia@gmail.com>
2018-09-12 17:25:22 +0200

    adds asPlDate String extension and changes in inCash Double extension

--- KursWalutNBP/Extensions/DoubleExtension.swift
+++ KursWalutNBP/Extensions/DoubleExtension.swift
@@ -8,12 +8,13 @@
 
 import Foundation
 
-var numberFormatter = NumberFormatter()
+//var numberFormatter = NumberFormatter()
 extension Double {
     var inCash: String {
         get {
+            let numberFormatter = NumberFormatter() //
             numberFormatter.numberStyle = .decimal
-            numberFormatter.maximumSignificantDigits = 2
+            //numberFormatter.maximumSignificantDigits = 3
             let cashValue = numberFormatter.string(from: NSNumber(value: self))!
             return cashValue + " PLN"
         }
---
+++ KursWalutNBP/Extensions/StringExtension.swift
@@ -0,0 +1,22 @@
+//
+//  StringExtension.swift
+//  KursWalutNBP
+//
+//  Created by Natalia Hetmańska on 12/09/2018.
+//  Copyright © 2018 npisze. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    
+    func asPlDate() -> String {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "pl")
+        formatter.dateFormat = "yyyy-MM-dd"
+        guard let newDate = formatter.date(from: self) else { return "no data" }
+        formatter.dateFormat = "d MMMM yyyy"
+        let newStringDate = formatter.string(from: newDate)
+        return newStringDate
+    }
+}
